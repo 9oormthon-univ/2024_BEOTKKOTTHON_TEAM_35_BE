55d127add98925f77561bc81f31a2895b3df0e62
kimsungmin1011 <tata153@naver.com>
2024-03-17 19:27:42 +0900

    검색 기능 Redis 캐싱 도입

--- src/main/java/com/example/demo/service/FinancialProductService.java
+++ src/main/java/com/example/demo/service/FinancialProductService.java
@@ -3,9 +3,11 @@ package com.example.demo.service;
 import com.example.demo.dto.FinancialProductsResponse;
 import com.example.demo.model.FinancialProduct;
 import com.example.demo.repository.FinancialProductRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -13,6 +15,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class FinancialProductService {
@@ -26,24 +29,25 @@ public class FinancialProductService {
     @Autowired
     private FinancialProductRepository financialProductRepository;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     public void fetchAndSaveFinancialProducts() {
-        // 인코딩된 API 키
         String encodedApiKey = URLEncoder.encode(apiKey, StandardCharsets.UTF_8);
-        // "청년"을 인코딩
-//        String encodedProductName = URLEncoder.encode("청년", StandardCharsets.UTF_8);
+        String encodedPrdNm = URLEncoder.encode("교보", StandardCharsets.UTF_8);
 
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(apiUrl)
-                .queryParam("serviceKey", encodedApiKey) // 인코딩된 키 사용
+                .queryParam("serviceKey", encodedApiKey)
                 .queryParam("pageNo", "1")
-                .queryParam("numOfRows", "2")
+                .queryParam("numOfRows", "10")
                 .queryParam("resultType", "json")
-                .queryParam("prdNm", "KB");
+                .queryParam("prdNm", encodedPrdNm);
 
-        // .build(true)를 호출하여 이미 인코딩된 상태임을 명시
         String urlTemplate = builder.build(true).toUriString();
 
         RestTemplate restTemplate = new RestTemplate();
-        ObjectMapper objectMapper = new ObjectMapper();
 
         try {
             String response = restTemplate.getForObject(urlTemplate, String.class);
@@ -51,23 +55,44 @@ public class FinancialProductService {
 
             if (productsResponse != null && productsResponse.getGetProductList() != null && productsResponse.getGetProductList().getItem() != null) {
                 List<FinancialProductsResponse.ProductList.Item> items = productsResponse.getGetProductList().getItem();
-
                 items.forEach(item -> {
                     FinancialProduct financialProduct = new FinancialProduct(item.getFncIstNm(), item.getPrdNm(), item.getPrdSalDscnDt(), item.getRegDate());
                     financialProductRepository.save(financialProduct);
                 });
             } else {
-                // 로그 남기기 또는 사용자 정의 예외 처리
                 System.out.println("No products found or error in response format.");
             }
         } catch (Exception e) {
             e.printStackTrace();
-            // 로그 남기기 또는 사용자 정의 예외 처리
             System.out.println("Error fetching financial products: " + e.getMessage());
-            // 필요에 따라 사용자 정의 예외를 던지거나 다른 방식으로 처리
         }
     }
+
     public List<FinancialProduct> searchFinancialProducts(String keyword) {
-        return financialProductRepository.findByFncIstNmOrPrdNmContaining(keyword);
+        String redisKey = "search:" + keyword;
+
+        // Redis에서 검색 결과 조회
+        String cachedProducts = stringRedisTemplate.opsForValue().get(redisKey);
+
+        if (cachedProducts != null) {
+            try {
+                return objectMapper.readValue(cachedProducts, new TypeReference<List<FinancialProduct>>() {});
+            } catch (Exception e) {
+                throw new RuntimeException("Error parsing cached financial products", e);
+            }
+        } else {
+            List<FinancialProduct> products = financialProductRepository.findByFncIstNmOrPrdNmContaining(keyword);
+
+            if (!products.isEmpty()) {
+                try {
+                    // 검색 결과를 Redis에 캐싱
+                    stringRedisTemplate.opsForValue().set(redisKey, objectMapper.writeValueAsString(products), 60, TimeUnit.MINUTES); // 60분 동안 캐시 유지
+                } catch (Exception e) {
+                    throw new RuntimeException("Error caching financial products", e);
+                }
+            }
+
+            return products;
+        }
     }
 }
