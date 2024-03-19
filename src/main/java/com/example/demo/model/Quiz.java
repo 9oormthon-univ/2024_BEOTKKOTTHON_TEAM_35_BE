54061073f508f6d8639bd8739a2d3390dbf2dafa
kimsungmin1011 <tata153@naver.com>
2024-03-19 19:19:39 +0900

    유저-퀴즈 의존성 주입

--- src/main/java/com/example/demo/model/Quiz.java
+++ src/main/java/com/example/demo/model/Quiz.java
@@ -1,25 +1,27 @@
 package com.example.demo.model;
 
+import com.example.demo.users.entity.User;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Data
-@NoArgsConstructor  // Lombok이 기본 생성자를 생성하도록 합니다.
-@AllArgsConstructor // Lombok이 모든 필드를 포함하는 생성자를 생성하도록 합니다.
+@NoArgsConstructor
+@AllArgsConstructor
 public class Quiz {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String question;
     private String correctAnswer;
     private String wrongAnswer1;
     private String wrongAnswer2;
-    private String category; // 금융 개념, 금융 상품, 금융 지표, 금융 기관
-    private String answerStatus; // 답변 상태: NULL (미응답), CORRECT (정답), WRONG (오답)
+    private String category; // 예: 금융 개념, 금융 상품 등
+    private String answerStatus; // 예: NULL (미응답), CORRECT (정답), WRONG (오답)
 
-    // Lombok이 제공하는 어노테이션을 사용하여 추가적인 생성자나 getter/setter를 명시적으로 작성할 필요가 없습니다.
+    // Quiz와 User 사이의 ManyToOne 관계를 정의
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
