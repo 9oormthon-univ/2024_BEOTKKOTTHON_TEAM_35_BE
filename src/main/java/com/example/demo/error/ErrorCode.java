5387512b3db7d454afbff989fe24879c5b7c7d31
sejongyundeokgeon <ydg0630@naver.com>
2024-03-22 16:32:38 +0900

    feat : 에러코드 추가

--- src/main/java/com/example/demo/error/ErrorCode.java
+++ src/main/java/com/example/demo/error/ErrorCode.java
@@ -9,7 +9,9 @@ public enum ErrorCode {
     //user
     USER_NOT_EXIST(500,"US01","USER NOT EXIST"),
     USER_PASSWORD_INCORRECT(500,"US02","USER PASSWORD INCORRECT"),
-    EXCHANGE_NOT_EXIST(500,"US03","EXCHANGE NOT EXIST")
+    EXCHANGE_NOT_EXIST(500,"US03","EXCHANGE NOT EXIST"),
+    //scholarship
+    SCHOLARSHIP_NOT_EXIST(500,"SC01","SCHOLARSHIP NOT EXIST")
     ;
     final private int status;
     final private String errorCode;
